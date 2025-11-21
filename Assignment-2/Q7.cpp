e280929dee6224f0b794d5a6ca07313ed5322e1a
DIVIYANSHU <dverma_be24@thapar.edu>
2025-11-21 19:20:20 +0530

    Create Q7.cpp

---
+++ Assignment-2/Q7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Function to count inversions
+int countInversions(int arr[], int n) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    int result = countInversions(arr, n);
+    cout << "Number of inversions = " << result << endl;
+
+    return 0;
+}
